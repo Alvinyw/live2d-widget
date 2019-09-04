29fc6543cc70fe1ca218190606f63456facffb00
zhousiwei <2434387555@qq.com>
2019-09-04 11:45:34 +0800

    更新JS代码

--- autoload.js
+++ autoload.js
@@ -1,48 +1,28 @@
-//注意：live2d_path参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";
-//const live2d_path = "/live2d-widget/";
+// 注意：live2d_path参数应使用绝对路径
+//const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/";
+const live2d_path = "./";
 
-//加载waifu.css
+// 获取script DOM元素
+var s = document.getElementsByTagName("script")[0]; 
+
+// 加载live2d.min.js
+var live2d = document.createElement("script");
+live2d.src = live2d_path + "live2d.min.js";
+//live2d.src = "https://zhousiwei.gitee.io/live2dw/lib/L2Dwidget.0.min.js";
+s.parentNode.insertBefore(live2d, s);
+
+// 加载waifu-tips.js
+var waifu_tips = document.createElement("script");
+waifu_tips.src = live2d_path + "waifu-tips.js";
+s.parentNode.insertBefore(waifu_tips, s);
+
+// 加载waifu.css
 $("<link>").attr({ href: live2d_path + "waifu.css", rel: "stylesheet" }).appendTo("head");
 
-//加载live2d.min.js
-$.ajax({
-	url: live2d_path + "live2d.min.js",
-	dataType: "script",
-	cache: true
-});
-
-//加载waifu-tips.js
-$.ajax({
-	url: live2d_path + "waifu-tips.js",
-	dataType: "script",
-	cache: true
-});
-
-//初始化看板娘，会自动加载指定目录下的waifu-tips.json
+// 初始化看板娘，会自动加载指定目录下的waifu-tips.json
 $(window).on("load", function() {
 	initWidget(live2d_path + "waifu-tips.json", "https://live2d.fghrsh.net/api");
 });
 //initWidget第一个参数为waifu-tips.json的路径
 //第二个参数为api地址（无需修改）
 //api后端可自行搭建，参考https://github.com/fghrsh/live2d_api
-
-console.log(`
-  く__,.ヘヽ.        /  ,ー､ 〉
-           ＼ ', !-─‐-i  /  /´
-           ／｀ｰ'       L/／｀ヽ､
-         /   ／,   /|   ,   ,       ',
-       ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
-        ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
-          !,/7 '0'     ´0iソ|    |
-          |.从"    _     ,,,, / |./    |
-          ﾚ'| i＞.､,,__  _,.イ /   .i   |
-            ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
-              | |/i 〈|/   i  ,.ﾍ |  i  |
-             .|/ /  ｉ：    ﾍ!    ＼  |
-              kヽ>､ﾊ    _,.ﾍ､    /､!
-              !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
-              ﾚ'ヽL__|___i,___,ンﾚ|ノ
-                  ﾄ-,/  |___./
-                  'ｰ'    !_,.:
-`);
